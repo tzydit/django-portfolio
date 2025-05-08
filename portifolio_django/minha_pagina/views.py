482548f67e6ab956525d00df95661af419e6fcd1
Miguel Castanho Reis <125430760+tzydit@users.noreply.github.com>
2025-05-08 12:08:48 -0300

    feat: add contact form with custom styling and navigation links

--- portifolio_django/minha_pagina/views.py
+++ portifolio_django/minha_pagina/views.py
@@ -1,4 +1,25 @@
 from django.shortcuts import render
+from .forms import ContatoForm  # <-- necessário
+from django.http import HttpResponse
+
+def contato_view(request):
+    enviado = False
+    dados = None
+
+    if request.method == 'POST':
+        form = ContatoForm(request.POST)
+        if form.is_valid():
+            dados = form.cleaned_data
+            enviado = True
+    else:
+        form = ContatoForm()
+
+    return render(request, 'minha_pagina/contato.html', {
+        'form': form,
+        'enviado': enviado,
+        'dados': dados
+    })
 
 def django_info(request):
     return render(request, 'minha_pagina/django_info.html')
+
