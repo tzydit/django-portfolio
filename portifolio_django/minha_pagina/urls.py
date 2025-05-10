115c2033d521fbc4a03c3a01463a52eb49400ad5
jornadev <jornadahenrique12@gmail.com>
2025-05-10 15:42:20 -0300

    feat: api viacep funcional

--- portifolio_django/minha_pagina/urls.py
+++ portifolio_django/minha_pagina/urls.py
@@ -4,5 +4,5 @@ from .views import django_info, contato_view, consulta_cep_view
 urlpatterns = [
     path('', django_info, name='home'),
     path('contato/', contato_view, name='contato'),
-    path('consulta-cep/', consulta_cep_view, name='consulta_cep'),
+    path('consulta_cep/', consulta_cep_view, name='cep'),
 ]
--- portifolio_django/minha_pagina/views.py
+++ portifolio_django/minha_pagina/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render
 from .forms import ContatoForm
 from django.http import HttpResponse
-from .utils import buscar_cep_via_api  # função para consumir a API do ViaCEP
-
+from .utils import buscar_cep_via_api 
 
 def contato_view(request):
     enviado = False
@@ -22,6 +21,18 @@ def contato_view(request):
         'dados': dados
     })
 
+def buscar_cep(cep):
+    url = f'https://viacep.com.br/ws/{cep}/json/'
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            return response.json()  
+        else:
+            return {"erro": "Erro na consulta do CEP."}  
+    except requests.exceptions.RequestException as e:
+        return {"erro": f"Erro ao conectar com a API: {str(e)}"} 
+
+
 
 def django_info(request):
     return render(request, 'minha_pagina/django_info.html')
@@ -30,7 +41,7 @@ def django_info(request):
 def consulta_cep_view(request):
     resultado = None
     if request.method == 'POST':
-        cep = request.POST.get('cep', '').replace('-', '').strip()
+        cep = request.POST.get('cep', '').replace('-', '').strip() 
         if cep:
-            resultado = buscar_cep_via_api(cep)
+            resultado = buscar_cep_via_api(cep)  
     return render(request, 'minha_pagina/consulta_cep.html', {'resultado': resultado})
