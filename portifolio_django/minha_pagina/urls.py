989405423ab1156c26b14aca7e7b89315a7a5367
Tazzoy <125430760+tzydit@users.noreply.github.com>
2025-05-13 15:35:17 -0300

    fix: duplicated view and ensure categories and frameworks render correctly

--- portifolio_django/minha_pagina/urls.py
+++ portifolio_django/minha_pagina/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from .views import django_info, contato_view, consulta_cep_view
-from .views import aplicacoes, documentacao
+from .views import aplicacoes, documentacao, filtrar_frameworks
+
 urlpatterns = [
     path('', django_info, name='home'),
     path('contato/', contato_view, name='contato'),
+    path('consulta_cep/', consulta_cep_view, name='cep'),
     path('documentacao/', documentacao, name='documentacao'),
     path('aplicacoes/', aplicacoes, name='aplicacoes'),
-    path('consulta_cep/', consulta_cep_view, name='cep'),
-
+    path('filtrar_frameworks/', filtrar_frameworks, name='filtrar_frameworks')
 ]
